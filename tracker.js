41c38f915c8abaa73d6cf19643ceecdfccbf2428
jetlime <paul.houssel@yahoo.de>
2021-04-23 11:24:06 +0200

    read all js files used in webpage

--- tracker.js
+++ tracker.js
@@ -1,1 +1,38 @@
-console.log("hello")
+
+fingerprinting_functions = [  
+    ".getUserMedia",
+    ".availHeight",
+    ".availWidth",
+    ".charCodeAt",
+    ".colorDepth",
+    ".getTimezoneOffset",
+    ".javaEnabled",
+    ".mimeType",
+    ".platform",
+    ".plugins",
+    ".propertyIsEnumerable",
+    ".referrer",
+    ".screen",
+    "canvas"
+]
+function checkKeywords(text) {
+    console.log(text)
+    if( fingerprinting_functions.some(keyword => text.includes(keyword) )) {
+        console.log("Found")
+    }
+}
+
+var jsFilePaths = Array.prototype.slice
+    .apply(document.querySelectorAll('script'))
+    .filter(s => s.src)
+    .map(s => s.src);
+
+for(let i=0; i < jsFilePaths.length ; i++){
+    console.log(i)
+    
+    fetch(jsFilePaths[i])
+    .then( r => r.text() )
+    .then( t => checkKeywords(t)
+    
+    )
+}
