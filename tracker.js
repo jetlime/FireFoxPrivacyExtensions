8ece688d4d6b46b7c5e8f97b8f9f6caa1cb96b63
jetlime <paul.houssel@yahoo.de>
2021-04-28 14:39:33 +0200

    find the js methods in the js files and logs them

--- tracker.js
+++ tracker.js
@@ -1,25 +1,30 @@
 
-fingerprinting_functions = [  
-    ".getUserMedia",
-    ".availHeight",
-    ".availWidth",
-    ".charCodeAt",
-    ".colorDepth",
-    ".getTimezoneOffset",
-    ".javaEnabled",
-    ".mimeType",
-    ".platform",
-    ".plugins",
-    ".propertyIsEnumerable",
-    ".referrer",
-    ".screen",
-    "canvas"
-]
+fingerprinting_functions = {
+    ".getUserMedia" : "Looks for used Device",
+    ".availHeight" : "Looks for screen Height",
+    ".availWidth": "Looks for screen width",
+    ".charCodeAt": "Looks for used font",
+    ".colorDepth": "Looks for color depth of your screen",
+    ".getTimezoneOffset": "Looks for your Time zone",
+    ".javaEnabled" : "Looks if you enabled Javascript",
+    ".mimeType": "Looks for mimeType",
+    ".platform" : "Looks for OS",
+    ".plugins": "Looks for your plugins",
+    ".propertyIsEnumerable": "Looks for config",
+    ".referrer": "Looks for referrer",
+    ".screen" : "Looks for your screen details",
+    "canvas": "Looks for canvas used in browser"
+}
+
+var span = document.getElementById("trackers")
+var JsList = document.getElementById("jslist")
+
 function checkKeywords(text) {
-    console.log(text)
-    if( fingerprinting_functions.some(keyword => text.includes(keyword) )) {
-        console.log("Found")
+    var matched = Object.keys(fingerprinting_functions).find(key => text.toLowerCase().search(key) > -1);
+    if(matched){
+        console.log(fingerprinting_functions[matched])
     }
+    
 }
 
 var jsFilePaths = Array.prototype.slice
@@ -28,11 +33,19 @@ var jsFilePaths = Array.prototype.slice
     .map(s => s.src);
 
 for(let i=0; i < jsFilePaths.length ; i++){
-    console.log(i)
-    
-    fetch(jsFilePaths[i])
+    jsfile = jsFilePaths[i]
+    console.log(jsFilePaths[i])
+    fetch(jsfile)
     .then( r => r.text() )
     .then( t => checkKeywords(t)
     
     )
 }
+/*
+for(let i=0; i < jsFilePaths.length ; i++){
+    jsfile = jsFilePaths[i]
+    var li = document.createElement("li")
+    li.appendChild(document.createTextNode(jsfile))
+    JsList.appendChild(li)
+}
+*/
